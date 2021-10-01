ffe81b8c0aaff1e99a6d39c5949773dabc33b402
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-10-01 12:12:48 +0530

    Added: Rolling The Die

--- src/main/java/org/blz/SnakeAndLadder/SnakeAndLadder.java
+++ src/main/java/org/blz/SnakeAndLadder/SnakeAndLadder.java
@@ -4,10 +4,13 @@ package org.blz.SnakeAndLadder;
 public class SnakeAndLadder
 {
     public static int START_POSITION = 0 ;
+
     public static void main( String[] args )
 
     {
         System.out.println( "Welcome To Snake And Ladder Simulator !" );
         System.out.println("The StartingPosition Of Player Is " + START_POSITION);
+        int diceNumber = (int)Math.floor(((Math.random()*10) % 7) + 1);
+        System.out.println("the Dice Number Is " + diceNumber);
     }
 }
