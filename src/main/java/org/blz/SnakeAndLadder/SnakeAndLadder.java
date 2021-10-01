9c530b4b3edac08b15c8017c2df54d5d68657294
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-10-01 21:54:33 +0530

    Added:Winning Position

--- src/main/java/org/blz/SnakeAndLadder/SnakeAndLadder.java
+++ src/main/java/org/blz/SnakeAndLadder/SnakeAndLadder.java
@@ -3,28 +3,51 @@ package org.blz.SnakeAndLadder;
 
 
 
-public class SnakeAndLadder
-{
-    public static int START_POSITION = 0 ;
-    public static int  NO_PLAY = 1;
+public class SnakeAndLadder {
+    public static int START_POSITION = 0;
+    public static int INITIAL_POSITION = 0;
+    public static int WINNING_POSITION = 100;
+    public static int NO_PLAY = 1;
     public static int LADDER = 2;
     public static int SNAKE = 3;
 
-    public static void main( String[] args )
 
-    {
-        System.out.println( "Welcome To Snake And Ladder Simulator !" );
+
+    public static void main(String[] args) {
+        int presentPosition = 0;
+        int nextPosition;
+        System.out.println("Welcome To Snake And Ladder Simulator !");
         System.out.println("The StartingPosition Of Player Is " + START_POSITION);
-        int diceNumber = (int)Math.floor(((Math.random()*10) % 6) + 1);
-        System.out.println("the Dice Number Is " + diceNumber);
-        int Option = (int)Math.floor(((Math.random()*10) % 3) + 1);
-        System.out.println(" the Option is " + Option );
-        if (Option == 1) {
-            System.out.println(" Present position Is"+ (START_POSITION + 0));
-        } else if (Option == 2) {
-            System.out.println("Present position Is " + (START_POSITION + diceNumber));
-        } if (Option == 3) {
-        System.out.println("Present position Is " + (START_POSITION  - diceNumber));
-    }
+
+        while (START_POSITION <= 99 ) {
+            int diceNumber = (int) Math.floor(((Math.random() * 10) % 6) + 1);
+            System.out.println("the Dice Number Is " + diceNumber);
+            int Option = (int) Math.floor(((Math.random() * 10) % 3) + 1);
+            System.out.println("The Option Is " + Option);
+            switch (Option) {
+                case 1:
+                    System.out.println( "presentPosition Is" + (START_POSITION + 0));
+                    break;
+
+
+                case 2:
+                    System.out.println("presentPosition Is" + (START_POSITION + diceNumber));
+
+                    break;
+
+                default:
+                    System.out.println("presentPosition Is" + (START_POSITION - diceNumber));
+                    if ((START_POSITION - diceNumber) < INITIAL_POSITION) {
+                        System.out.println(START_POSITION = (INITIAL_POSITION ));
+                    }
+
+
+
+            }
+            START_POSITION++;
+
+
+        }
+
     }
 }
