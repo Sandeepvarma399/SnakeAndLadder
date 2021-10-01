aaabb4afe0d5ae0a48907896837c054e182723c8
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-10-01 11:52:45 +0530

    Added:Starting Position Of Player

--- src/main/java/org/blz/SnakeAndLadder/SnakeAndLadder.java
+++ src/main/java/org/blz/SnakeAndLadder/SnakeAndLadder.java
@@ -3,9 +3,11 @@ package org.blz.SnakeAndLadder;
 
 public class SnakeAndLadder
 {
+    public static int START_POSITION = 0 ;
     public static void main( String[] args )
 
     {
         System.out.println( "Welcome To Snake And Ladder Simulator !" );
+        System.out.println("The StartingPosition Of Player Is " + START_POSITION);
     }
 }
